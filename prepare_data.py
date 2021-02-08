c3df592d9ce325c603190a7d19b564ebfc792821
linmin001 <linmin001@e.ntu.edu.sg>
2021-02-08 10:38:30 -0500

    data pipeline fully done

--- prepare_data.py
+++ prepare_data.py
@@ -9,6 +9,7 @@ import rdkit
 import scipy
 import multiprocessing
 
+from functools import partial
 from datetime import datetime
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
@@ -38,11 +39,12 @@ def mol_smi_to_count_fp(
 # use fp_size = 32681 and radius = 2 for now
 # TODO: fold to 1,000,000-dim --> log(x+1) --> variance threshold into 32681
 def gen_prod_fps(args):
+    # parallelizing makes it very slow for some reason
     for phase in ['train', 'valid', 'test']:
         logging.info(f'Processing {phase}')
 
-        with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as handle:
-            clean_rxnsmi_phase = pickle.load(handle)
+        with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as f:
+            clean_rxnsmi_phase = pickle.load(f)
 
         phase_prod_smi_nomap = []
         phase_rxn_prod_fps = []
@@ -65,13 +67,13 @@ def gen_prod_fps(args):
             phase_rxn_prod_fps
         )
 
-        with open(args.data_folder / f"{args.output_file_prefix}_prod_smis_{phase}.smi") as f:
+        with open(args.data_folder / f"{args.output_file_prefix}_prod_smis_nomap_{phase}.smi", 'wb') as f:
             pickle.dump(phase_prod_smi_nomap, f, protocol=4)
 
 def get_tpl(task):
     idx, react, prod = task
     reaction = {'_id': idx, 'reactants': react, 'products': prod}
-    template = extract_from_reaction(reaction, super_general=True)
+    template = extract_from_reaction(reaction)
     # https://github.com/connorcoley/rdchiral/blob/master/rdchiral/template_extractor.py
     return idx, template
 
@@ -93,9 +95,9 @@ def get_train_templates(args):
     52% and 79% of all chemical reactions from 2015 and after, respectively.
     '''
     logging.info('Extracting templates from training data')
-    phase == 'train'
-    with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as handle:
-        clean_rxnsmi_phase = pickle.load(handle)
+    phase = 'train'
+    with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as f:
+        clean_rxnsmi_phase = pickle.load(f)
 
     templates = {}
     rxns = []
@@ -103,7 +105,7 @@ def get_train_templates(args):
         r = rxn_smi.split('>>')[0]
         p = rxn_smi.split('>>')[-1]
         rxns.append((idx, r, p))
-    print('total training rxns:', len(rxns))
+    logging.info(f'Total training rxns: {len(rxns)}')
 
     num_cores = len(os.sched_getaffinity(0))
     logging.info(f'Parallelizing over {num_cores} cores')
@@ -112,8 +114,14 @@ def get_train_templates(args):
                         total=len(rxns)):
         idx, template = result
         if 'reaction_smarts' not in template:
-            continue
-        cano_temp = cano_smarts(template['reaction_smarts'])
+            continue # no template could be extracted
+
+        # canonicalize template
+        p_temp = cano_smarts(template['products']) # reaction_smarts
+        r_temp = cano_smarts(template['reactants'])
+        cano_temp = r_temp + '>>' + p_temp
+        # cano_temp = cano_smarts(template['reaction_smarts'])
+
         if cano_temp not in templates:
             templates[cano_temp] = 1
         else:
@@ -124,45 +132,59 @@ def get_train_templates(args):
     with open(args.data_folder / args.templates_file, 'w') as f:
         f.writelines(templates)
 
+def get_template_idx(temps_filtered, task):
+    # extract template for this rxn_smi, and match it to template dictionary from training data
+    rxn_idx, rxn_smi = task
+    r = rxn_smi.split('>>')[0]
+    p = rxn_smi.split('>>')[-1]
+    rxn = (rxn_idx, r, p)
+    rxn_idx, rxn_template = get_tpl(rxn)
+    p_temp = cano_smarts(rxn_template['products']) # reaction_smarts
+    r_temp = cano_smarts(rxn_template['reactants'])
+    cano_temp = r_temp + '>>' + p_temp
+
+    # rxn_mol = Chem.MolFromSmiles(rxn_smi)
+    # [a.SetAtomMapNum(0) for a in rxn_mol.GetAtoms()]
+
+    for temp_idx, extr_temp in enumerate(temps_filtered):
+        if extr_temp == cano_temp:
+            return rxn_idx, temp_idx # template_idx = label
+
+    # for temp_idx, pattern in enumerate(temps_filtered):
+    #     pattern_mol = Chem.MolFromSmarts(pattern)
+    #     if pattern_mol is None:
+    #         logging.info('error: pattern_mol is None')
+    #     try:
+    #         matches = rxn_mol.GetSubstructMatches(
+    #                         pattern_mol,
+    #                         useChirality=True
+    #                     ) # TODO: should this be False? (in RetroXpert it's False)
+    #     except:
+    #         continue
+    #     else:
+    #         if len(matches) > 0:
+    #             return rxn_idx, temp_idx # template_idx = label
+    # logging.info(rxn_smi)
+    return rxn_idx, 9999 # no template matching
+
 def match_templates(args):
-    logging.info('Loading templates from file:', args.template_file)
+    logging.info(f'Loading templates from file: {args.templates_file}')
     with open(args.data_folder / args.templates_file, 'r') as f:
         templates = f.readlines()
     temps_filtered = []
-    for p in patterns:
+    for p in templates:
         pa, cnt = p.strip().split(': ')
         if int(cnt) >= args.min_freq:
             temps_filtered.append(pa)
-    logging.info('total number of template patterns:', len(temps_filtered))
-
-    def find_template(task):
-        rxn_idx, rxn_smi = task
-        rxn_mol = Chem.MolFromSmiles(rxn_smi)
-        [a.SetAtomMapNum(0) for a in rxn_mol.GetAtoms()]
-
-        for idx, pattern in enumerate(temps_filtered):
-            pattern_mol = Chem.MolFromSmarts(pattern)
-            if pattern_mol is None:
-                logging.info('error: pattern_mol is None')
-            try:
-                matches = rxn_mol.GetSubstructMatches(pattern_mol,
-                                                    useChirality=True) # TODO: should this be False? (in RetroXpert it's False)
-            except:
-                continue
-            else:
-                if len(matches) > 0:
-                    return k, idx
-        logging.info(rxn_smi)
-        return rxn_idx, 9999 # no template matching
-
-    with open(args.data_folder / f"{args.output_file_prefix}_prod_smis_{phase}.smi") as f:
-        phase_prod_smi_nomap = pickle.load(f)
+    logging.info(f'Total number of template patterns: {len(temps_filtered)}')
 
     logging.info('Matching against extracted templates')
     for phase in ['train', 'valid', 'test']:
         logging.info(f'Processing {phase}')
-        with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as handle:
-            clean_rxnsmi_phase = pickle.load(handle)
+        with open(args.data_folder / f"{args.output_file_prefix}_prod_smis_nomap_{phase}.smi", 'rb') as f:
+            phase_prod_smi_nomap = pickle.load(f)
+        with open(args.data_folder / f'{args.rxnsmi_file_prefix}_{phase}.pickle', 'rb') as f:
+            clean_rxnsmi_phase = pickle.load(f)
         
         tasks = [
             (idx, rxn_smi) for idx, rxn_smi in enumerate(clean_rxnsmi_phase)
@@ -176,7 +198,8 @@ def match_templates(args):
         col_names = ['rxn_idx', 'prod_smi', 'rcts_smi', 'temp_idx', 'template']
         rows = []
         found = 0
-        for result in tqdm(pool.imap_unordered(find_template, tasks),
+        get_template_partial = partial(get_template_idx, temps_filtered)
+        for result in tqdm(pool.imap_unordered(get_template_partial, tasks),
                        total=len(tasks)):
             rxn_idx, template_idx = result
 
@@ -187,12 +210,13 @@ def match_templates(args):
             # Sometimes stereochem takes another canonicalization...
             rcts_smi_nomap = Chem.MolToSmiles(Chem.MolFromSmiles(rcts_smi_nomap), True)
 
+            template = temps_filtered[template_idx] if template_idx != 9999 else ''
             rows.append([
                 rxn_idx,
                 phase_prod_smi_nomap[rxn_idx],
                 rcts_smi_nomap,
                 template_idx,
-                temps_filtered[template_idx]
+                template
             ])
             found += (template_idx != 9999)
         
@@ -212,8 +236,60 @@ pattern_feat feature dim:  646
 # ave center per mol: 36
 '''
 
+def parse_args():
+    parser = argparse.ArgumentParser("prepare_data.py")
+    # file names
+    parser.add_argument("--log_file", help="log_file", type=str, default="prepare_data")
+    parser.add_argument("--data_folder", help="Path to data folder (do not change)", type=str,
+                        default=None) 
+    parser.add_argument("--rxnsmi_file_prefix", help="Prefix of the 3 pickle files containing the train/valid/test reaction SMILES strings (do not change)", type=str,
+                        default='50k_clean_rxnsmi_noreagent_allmapped_canon') 
+    parser.add_argument("--output_file_prefix", help="Prefix of output files", 
+                        type=str)
+    parser.add_argument("--templates_file", help="Filename of templates extracted from training data", 
+                        type=str, default='50k_training_templates')
+    # parser.add_argument("--parallelize", help="Whether to parallelize over all available cores", action="store_true")
+    
+    parser.add_argument("--min_freq", help="Minimum frequency of template in training data to be retained", type=int, default=1)
+    parser.add_argument("--radius", help="Fingerprint radius", type=int, default=2)
+    parser.add_argument("--fp_size", help="Fingerprint size", type=int, default=32681)
+    # parser.add_argument("--fp_type", help='Fingerprint type ["count", "bit"]', type=str, default="count")
+    return parser.parse_args()
+
 if __name__ == '__main__':
     # NOTE: rmbr to use allmapped_canon_{phase}.pickle
+    args = parse_args()
+ 
+    RDLogger.DisableLog("rdApp.warning")
+    os.makedirs("./logs", exist_ok=True)
+    dt = datetime.strftime(datetime.now(), "%y%m%d-%H%Mh")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO) 
+    fh = logging.FileHandler(f"./logs/{args.log_file}.{dt}")
+    fh.setLevel(logging.INFO)
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
 
-    # TODO: parse args
-    # setup args.data_folder
+    if args.data_folder is None:
+        args.data_folder = Path(__file__).resolve().parents[0] / 'data'
+    else:
+        args.data_folder = Path(args.data_folder)
+
+    if args.output_file_prefix is None:
+        args.output_file_prefix = f'50k_{args.fp_size}dim_{args.radius}rad'
+
+    logging.info(args)
+
+    if not (args.data_folder / f"{args.output_file_prefix}_prod_fps_valid.npz").exists():
+        # ~2 min on 40k train prod_smi on 16 cores
+        gen_prod_fps(args)
+    if not (args.data_folder / args.templates_file).exists():
+        # ~40 sec on 40k train rxn_smi on 16 cores
+        get_train_templates(args)
+    if True : # not (args.data_folder / f"{args.output_file_prefix}_csv_train.csv").exists():
+        # ~3 min on 40k train rxn_smi on 16 cores
+        match_templates(args)
+    
+    logging.info('Done!')
--- train.py
+++ train.py
@@ -10,26 +10,28 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
 from datetime import datetime
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
 from scipy import sparse
 from tqdm import tqdm
+from rdkit import RDLogger
 
 from model import TemplateNN
 from dataset import FingerprintDataset
 
 def train(args):
     # TODO: add GPU support, add test, display statistics
+    # TODO: add early stopping
     # TODO: add checkpointing & resuming from checkpoint
+    
     model = TemplateNN(
         output_size=args.template_count,
         size=args.hidden_size,
         num_layers_body=args.depth,
         input_size=args.fp_size
     )
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss() # should set reduction = 'sum' & then divide loss at end of epoch
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     train_dataset = FingerprintDataset(
@@ -80,8 +82,54 @@ def test(model, args):
 
     logging.info('Finished Testing')
 
+def parse_args():
+    parser = argparse.ArgumentParser("train.py")
+    # mode & metadata
+    parser.add_argument("--expt_name", help="experiment name", type=str, default="")
+    # parser.add_argument("--date_trained", help="date trained (DD_MM_YYYY)", 
+    #                     type=str, default=date.today().strftime("%d_%m_%Y"))
+    # file names
+    parser.add_argument("--log_file", help="log_file", type=str, default="train")
+    parser.add_argument("--prodfps_file_prefix",
+                        help="npz file of product fingerprints",
+                        type=str)
+    parser.add_argument("--labels_file_prefix",
+                        help="csv file of labels",
+                        type=str)
+
+    parser.add_argument("--radius", help="Fingerprint radius", type=int, default=2)
+    parser.add_argument("--fp_size", help="Fingerprint size", type=int, default=32681)
+    # parser.add_argument("--fp_type", help='Fingerprint type ["count", "bit"]', type=str, default="count")
+    # training params
+    parser.add_argument("--checkpoint", help="whether to save model checkpoints", action="store_true")
+    parser.add_argument("--random_seed", help="random seed", type=int, default=0)
+    
+    return parser.parse_args()
+
 if __name__ == '__main__':
+    args = parse_args()
+ 
+    RDLogger.DisableLog("rdApp.warning")
+    os.makedirs("./logs", exist_ok=True)
+    dt = datetime.strftime(datetime.now(), "%y%m%d-%H%Mh")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO) 
+    fh = logging.FileHandler(f"./logs/{args.log_file}.{dt}")
+    fh.setLevel(logging.INFO)
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+
+    if args.data_folder is None:
+        args.data_folder = Path(__file__).resolve().parents[0] / 'data'
+    else:
+        args.data_folder = Path(args.data_folder)
+
+    if args.labels_file_prefix is None:
+        args.labels_file_prefix = f'50k_{args.fp_size}dim_{args.radius}rad_csv'
+    if args.prodfps_file_prefix is None:
+        args.prodfps_file_prefix = f'50k_{args.fp_size}dim_{args.radius}rad_prod_fps'
+
     model = train(args)
     test(model, args)
-
-    # TODO: parse args
